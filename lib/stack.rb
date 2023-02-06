56de178efbc2834a542520f038e428c875a37812
Tyler Lampel <lampel13@gmail.com>
2023-02-05 19:30:15 -0500

    commit tree a9ce596fb2df83c9636ba0929244e00162551346

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,41 @@
-# your code here
+class Stack
+
+    def initialize (limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(item)
+        raise "Stack Overflow" if full?
+
+        @stack.push(item)
+    end
+
+    def size
+        @stack.length
+    end
+
+    def pop
+        @stack.pop 
+    end
+
+    def search(target)
+        @stack.each_with_index do |item, index|
+            return size - index -1 if item == target
+        end
+
+        -1
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full?
+        @limit && @stack.size == @limit
+    end
+end
